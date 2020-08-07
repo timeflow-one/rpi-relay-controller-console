e1e78b4fd264f4c52ea4c2634f0f1066ee489a87
Alexander Kondrev <kondrev@trinity.local>
2020-08-07 13:40:06 +0300

    Add add lock component

---
+++ src/ui/components/AddLockComponent.js
@@ -0,0 +1,33 @@
+import { Component, Vue, Watch } from 'vue-property-decorator'
+import LocksStore from '@/store/LocksStore'
+import RelaysStore from '@/store/RelaysStore'
+
+@Component
+export default class AddLockComponent extends Vue {
+  selectedLockType = -1
+  selectedSite = -1
+  inputDoorIdentificator = ''
+  selectedRelay = -1
+
+  get sites () {
+    return []
+  }
+
+  get relays () {
+    return RelaysStore.relays
+  }
+
+  get lockTypes () {
+    return LocksStore.types
+  }
+
+  /**
+   * @param {Array<import('@/models/LockTypeModel').LockTypeModel>} values
+   */
+  @Watch('lockTypes')
+  onLockTypesFilled (values) {
+    if (values.length === 1) {
+      this.selectedLockType = values[0].id
+    }
+  }
+}
---
+++ src/ui/components/AddLockComponent.vue
@@ -0,0 +1,74 @@
+<template>
+  <v-row>
+    <v-col
+      cols="12"
+      sm="6"
+      lg="3"
+    >
+      <v-subheader v-text="$vuetify.lang.t('$vuetify.locks.site_label')" />
+      <v-autocomplete
+        v-model="selectedSite"
+        :items="sites"
+        :label="$vuetify.lang.t('$vuetify.locks.select_site_label')"
+        :title="$vuetify.lang.t('$vuetify.locks.select_site_label')"
+        solo
+        hide-details
+      />
+    </v-col>
+
+    <v-col
+      cols="12"
+      sm="6"
+      lg="3"
+    >
+      <v-subheader v-text="$vuetify.lang.t('$vuetify.locks.door_label')" />
+      <v-text-field
+        v-model="inputDoorIdentificator"
+        :label="$vuetify.lang.t('$vuetify.locks.input_door_label')"
+        :title="$vuetify.lang.t('$vuetify.locks.input_door_label')"
+        solo
+        clearable
+        hide-details
+      />
+    </v-col>
+
+    <v-col
+      cols="12"
+      sm="6"
+      lg="3"
+    >
+      <v-subheader v-text="$vuetify.lang.t('$vuetify.locks.lock_type_label')" />
+      <v-select
+        v-model="selectedLockType"
+        :items="lockTypes"
+        item-value="id"
+        item-text="type"
+        :disabled="lockTypes.length <= 1"
+        :label="$vuetify.lang.t('$vuetify.locks.select_lock_type_label')"
+        :title="$vuetify.lang.t('$vuetify.locks.select_lock_type_label')"
+        solo
+        hide-details
+      />
+    </v-col>
+
+    <v-col
+      cols="12"
+      sm="6"
+      lg="3"
+    >
+      <v-subheader v-text="$vuetify.lang.t('$vuetify.locks.relay_label')" />
+      <v-autocomplete
+        v-model="selectedRelay"
+        :items="relays"
+        item-value="id"
+        item-text="gpio"
+        :label="$vuetify.lang.t('$vuetify.locks.select_relay_label')"
+        :title="$vuetify.lang.t('$vuetify.locks.select_relay_label')"
+        solo
+        hide-details
+      />
+    </v-col>
+  </v-row>
+</template>
+
+<script src="@/ui/components/AddLockComponent" />
